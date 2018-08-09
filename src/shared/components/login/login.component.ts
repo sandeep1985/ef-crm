b4ddece2441dedd21dd68dd8ed60cb3e12a40c6b
MKamaal <mkamalkhan9@yahoo.com>
2018-08-09 14:37:13 -0400

    [Login]:Dynamic validation added

--- src/shared/components/login/login.component.ts
+++ src/shared/components/login/login.component.ts
@@ -15,6 +15,8 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   model = new Login('', '');
   isLoggedIn : any;
+  isSubmit : Boolean;
+  isError : Boolean = false;
   constructor(private _userService : UserService, private _router : Router ) {
 
   }
@@ -34,10 +36,12 @@ ngOnInit() {
    }else{
     this._router.navigateByUrl('/login');
    }
+   this.isSubmit = false;
    console.log(window.location,'&&&&&&&&&&&');
 }
 
 onSubmit(){
+  this.isSubmit = true;
     this._userService.login(this.model.email, this.model.password)
     .subscribe((data :any) =>  {
       if(!data.error){
@@ -45,6 +49,7 @@ onSubmit(){
         window.location.href = window.location.origin + '/dashboard';
         //this._router.navigateByUrl('/dashboard');
       }else{
+        this.isError = true;
         console.log('Wrong');
       }
     });
